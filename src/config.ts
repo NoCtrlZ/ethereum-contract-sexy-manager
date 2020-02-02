ad5bdd13b1725df17a0ab671246a56f9d035eb21
Hemlocked <phantomofrotten@gmail.com>
2020-02-02 23:34:05 +0900

    version 0.1.2

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   name: "ethereum-contract-sexy-manager",
-  version: "0.1.1",
+  version: "0.1.2",
   networks: {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
